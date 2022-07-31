147d4afac4afe5f97b2af48547eb843a9b807c4e
vasil-yordanov14 <vasil_sh@abv.bg>
2022-07-31 22:40:00 +0300

    stock photos

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}?client_id=oBJveT1UGLBeZoZXE8U7CY67wihijgpbCcbO8GlfV8c`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  return <h2>stock photos starter</h2>;
+}
+
+export default App;
